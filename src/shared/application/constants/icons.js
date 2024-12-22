a47c4216052cdadb0a099fd5f273ba1930b52023
oscar Info <oscar.giraldo@ingeneosas282.onmicrosoft.com>
2024-12-22 10:41:01 -0500

    update ui card product

--- src/shared/application/constants/icons.js
+++ src/shared/application/constants/icons.js
@@ -4,3 +4,4 @@ export const TWITTER_ICON = 'ri-twitter-fill';
 export const TRANSLATE_ICON = 'ri-translate-2';
 export const TAG_PRICE_ICON = 'ri-price-tag-3-line';
 export const MENU_ICON = 'ri-menu-2-line';
+export const ARROW_RIGHT_ICON = 'ri-arrow-right-s-line';
--- src/shared/presentation/components/ProductCard/index.jsx
+++ src/shared/presentation/components/ProductCard/index.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
+import { PropTypes } from 'prop-types';
+import './ProductCard.scss';
+import { ARROW_RIGHT_ICON } from '../../../application/constants/icons';
+import { history } from '../../../application/helpers/history';
+import { formatNumberWithDots } from '../../../application/helpers/common-functions';
 
-const ProductCard = () => {
+const ProductCard = ({ isMini = false, urlRedirection, productImg, productName, productPrice }) => {
+	const handleRedirection = () => {
+		history.push(urlRedirection);
+	};
+
 	return (
-		<div className="product-card">
+		<div className={`product-card-container ${isMini ? 'mini' : ''}`}>
 			<div className="product-card__image">
-				<img src="https://via.placeholder.com/150" alt="Product" />
+				<img className="product-img" src={`${productImg || 'https://via.placeholder.com/150'}`} alt="Product" />
+				{!isMini && (
+					<div className="product-card__info">
+						<div className="text-container">
+							<h3 className="product-name">Product name</h3>
+							<p className="price">{`$ ${formatNumberWithDots('8000000')}`}</p>
+						</div>
+						<i className={`${ARROW_RIGHT_ICON} icon-redirection`} onClick={handleRedirection} />
+					</div>
+				)}
 			</div>
-			<div className="product-card__info">
-				<h3>Product name</h3>
-				<p>Product price</p>
-			</div>
+			{isMini && (
+				<div className="product-card-mini__info">
+					<p className="product-name">Product N</p>
+				</div>
+			)}
 		</div>
 	);
+};
+
+ProductCard.propTypes = {
+	isMini: PropTypes.bool,
+	urlRedirection: PropTypes.string,
+	productImg: PropTypes.string,
+	productName: PropTypes.string,
+	productPrice: PropTypes.string,
 };
 
 export default ProductCard;
