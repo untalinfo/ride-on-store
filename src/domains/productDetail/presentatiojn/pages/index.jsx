4c8e793b8f1357399014128bc65c04b2cd00cab6
oscar Info <racso1607@gmail.com>
2024-12-22 14:00:21 -0500

    add component button

--- src/domains/productDetail/presentatiojn/pages/index.jsx
+++ src/domains/productDetail/presentatiojn/pages/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import './ProductDetailPage.scss';
+import Button from '../../../../shared/presentation/components/Button';
+import { BANK_CARD_ICON } from '../../../../shared/application/constants/icons';
 
 const ProductDetailPage = () => {
 	return (
 		<div className="product-detail-page">
 			<h1>Product Detail</h1>
 			{/* Add your product detail components here */}
+			<Button label="Buy Now" leftIcon={BANK_CARD_ICON} />
 		</div>
 	);
 };
--- src/shared/application/constants/icons.js
+++ src/shared/application/constants/icons.js
@@ -5,3 +5,4 @@ export const TRANSLATE_ICON = 'ri-translate-2';
 export const TAG_PRICE_ICON = 'ri-price-tag-3-line';
 export const MENU_ICON = 'ri-menu-2-line';
 export const ARROW_RIGHT_ICON = 'ri-arrow-right-s-line';
+export const BANK_CARD_ICON = 'ri-bank-card-fill';
