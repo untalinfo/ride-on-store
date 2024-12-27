ed2dc645c494650f96cc06de6d532eec4d7a318f
oscar Info <racso1607@gmail.com>
2024-12-27 12:06:29 -0500

    fix config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import babelParser from "babel-eslint";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
